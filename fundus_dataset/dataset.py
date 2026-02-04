66a3507ec7ecaa85e3026575b826f222750bbd0c
grod23 <gabe7rodriguez@gmail.com>
2026-02-03 22:55:34 -0800

    Implemented visuals.py with class Visualizer() meant to handle plotting and displaying all the results from testing the model.

--- fundus_dataset/dataset.py
+++ fundus_dataset/dataset.py
@@ -21,6 +21,12 @@ class FundusDataset(Dataset):
             batch = self.transform(batch)
         image = batch[IMAGE_KEY]
         mask = batch[MASK_KEY]
+
+        plt.figure(figsize=(10, 10))
+        plt.title('Original Image')
+        plt.axis('off')
+        plt.imshow(image.permute(1, 2, 0).detach().cpu().numpy() / 255)
+        plt.show()
         # print(f'Image Shape: {image.shape}')
         # print(f'Image Type: {image.dtype}')
         # print("Image min/max:", image.min(), image.max())
@@ -29,29 +35,17 @@ class FundusDataset(Dataset):
         # print("Mask min/max:", mask.min(), mask.max())
         # print('\n')
 
-        # Image Preprocessing
-        # plt.figure(figsize=(10, 10))
-        # plt.subplot(1, 2, 1)
-        # plt.title('Image Before Preprocessing')
-        # plt.axis('off')
-        # plt.imshow(image.permute(1, 2, 0).detach().cpu().numpy() / 255.0)
-        # plt.subplot(1, 2, 2)
-        # plt.title('Mask Before Preprocessing')
-        # plt.axis('off')
-        # plt.imshow(mask.permute(1, 2, 0).detach().cpu().numpy() / 255.0)
-        # plt.show()
-
         # Extract green plane from image
         green_plane = image[1, :, :]
         # print(f'Green Shape: {green_plane.shape}')
         # print("Green min/max:", green_plane.min(), green_plane.max())
         # print(f'Green Type: {type(green_plane)}')
 
-        # plt.figure(figsize=(10, 10))
-        # plt.title('Green Plane')
-        # plt.axis('off')
-        # plt.imshow(green_plane.detach().cpu().numpy())
-        # plt.show()
+        plt.figure(figsize=(10, 10))
+        plt.title('Image Green Plane Only')
+        plt.axis('off')
+        plt.imshow(green_plane.detach().cpu().numpy())
+        plt.show()
 
         # CLAHE (Contrast Limited Adaptive Histogram Equalization)
         clahe = cv2.createCLAHE(
@@ -62,11 +56,11 @@ class FundusDataset(Dataset):
         green_np = green_plane.cpu().numpy().round().astype('uint8')
         enhanced_image = clahe.apply(green_np)
 
-        # plt.figure(figsize=(10, 10))
-        # plt.title('CLAHE Image')
-        # plt.axis('off')
-        # plt.imshow(enhanced_image)
-        # plt.show()
+        plt.figure(figsize=(10, 10))
+        plt.title('Image After CLAHE')
+        plt.axis('off')
+        plt.imshow(enhanced_image)
+        plt.show()
 
         # Mask Preprocessing:
         # Convert to single channel
@@ -85,16 +79,16 @@ class FundusDataset(Dataset):
         # Add channel dimension
         mask = mask.unsqueeze(0) # [Batch, Channel, Height, Width]
 
-        # plt.figure(figsize=(10, 10))
-        # plt.subplot(1, 2, 1)
-        # plt.title('Image After Preprocessing')
-        # plt.axis('off')
-        # plt.imshow(enhanced_image)
-        # plt.subplot(1, 2, 2)
-        # plt.title('Mask After Preprocessing')
-        # plt.axis('off')
-        # plt.imshow(mask.detach().cpu().numpy().squeeze(0) * 255)
-        # plt.show()
+        plt.figure(figsize=(10, 10))
+        plt.subplot(1, 2, 1)
+        plt.title('Image After Preprocessing')
+        plt.axis('off')
+        plt.imshow(enhanced_image)
+        plt.subplot(1, 2, 2)
+        plt.title('Mask After Preprocessing')
+        plt.axis('off')
+        plt.imshow(mask.detach().cpu().numpy().squeeze(0) * 255)
+        plt.show()
 
         # Normalize to [0, 1]
         enhanced_image = enhanced_image / 255.0
